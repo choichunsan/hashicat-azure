06da2587ea8a462d29c21b210d2cb59c7d5e1b99
choichunsan <kaucuro@megazone.com>
2023-12-28 05:27:49 +0000

    network.tf add

--- network.tf
+++ network.tf
@@ -2,7 +2,7 @@ module "network" {
   source  = "Azure/network/azurerm"
   version = "5.3.0"
   # insert the 2 required variables here
-  resource_group_name = "springtime-azure"
+  resource_group_name = "acctvnet"
   use_for_each = false
   resource_group_location = "East US"
 }
