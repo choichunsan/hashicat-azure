0fba608cf260d6c6ee2283e4083866622e271f83
choichunsan <kaucuro@megazone.com>
2023-12-28 04:59:20 +0000

    network.tf add

--- network.tf
+++ network.tf
@@ -1,7 +1,7 @@
 module "network" {
-  source  = "Azure/network/azurerm"
+  source  = "app.terraform.io/springtime-azure/network/azurerm"
   version = "3.5.0"
   # insert the 2 required variables here
   resource_group_name = "springtime"
-  resource_group_location = "East US"
+#   resource_group_location = "East US"
 }
